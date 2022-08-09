bb18adde0e007d9dc2e1e31ac2c70c977b909673
Milot Mirdita <milot@mirdita.de>
2022-08-10 00:53:14 +0900

    Disable jit in windows, since windows-abi function calls are not implemented

--- src/exprjit.c
+++ src/exprjit.c
@@ -1159,7 +1159,8 @@ double ej_interp(const char *str, int* error) {
   return result;
 }
 
-#if defined(__x86_64__) || defined(__arm64__)
+// windows function calls are not implemented, only sysv abi is supported
+#if (defined(__x86_64__) || defined(__arm64__)) && !defined(_WIN32)
 // #define DASM_CHECKS 1
 #include "dynasm/dasm_proto.h"
 #if defined(__arm64__)
